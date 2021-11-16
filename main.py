56d8c5c18c81f84f56f499b4233438767c2dc379
slackbot115 <viniccius.carvalho22@gmail.com>
2021-11-16 20:24:24 -0300

    Loops working

--- main.py
+++ main.py
@@ -1,61 +1,67 @@
 import re
 
-op = int(input("Digite a opcao que deseja realizar:\n1 - Verificar IPs\n2 - Verificar DDD\n3 - Verificar CPF\n"))
-
-if op == 1:
-    regex = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
-
-    insertBy = int(input("Deseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n"))
-
-    if insertBy == 1:
-        check_str = str(input("Digite o endereco IP: "))
-
-        matches = re.finditer(regex, check_str, re.MULTILINE)
+leave = False
+while(leave is not True):
+    op = int(input("\nDigite a opcao que deseja realizar:\n1 - Verificar IPs\n2 - Verificar DDD\n3 - Verificar CPF\n0 - Fechar\n"))
+    
+    if op == 1:
+        regex = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    
+        a = True
+        while(a):
+            insertBy = int(input("\nDeseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n0 - Para voltar\n"))
+            if insertBy == 1:
+                check_str = str(input("Digite o endereco IP: "))
         
-        for matchNum, match in enumerate(matches, start=1):
-            print ("IP valido: {match}".format(match = match.group()))
-            exit()
-        print("IP invalido...")
-
-    if insertBy == 2:
-        for i, line in enumerate(open("ips.txt")):
-            for match in re.finditer(regex, line):
-                print("IP identificado: {match}".format(match = match.group()))
-if op == 2:
-    regex = r"^(\([0-9]{2}\)|[0-9]{2})$"
-
-    insertBy = int(input("Deseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n"))
-
-    if insertBy == 1:
-        check_str = str(input("Digite o DDD: "))
-
-        matches = re.finditer(regex, check_str, re.MULTILINE)
+                matches = re.finditer(regex, check_str, re.MULTILINE)
+                
+                for matchNum, match in enumerate(matches, start=1):
+                    print ("IP valido: {match}\n".format(match = match.group()))
+            if insertBy == 2:
+                for i, line in enumerate(open("ips.txt")):
+                    for match in re.finditer(regex, line):
+                        print("IP identificado: {match}".format(match = match.group()))
+            if insertBy == 0:
+                break
+    if op == 2:
+        regex = r"^(\([0-9]{2}\)|[0-9]{2})$"
+    
+        a = True
+        while(a):
+            insertBy = int(input("\nDeseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n0 - Para voltar\n"))
         
-        for matchNum, match in enumerate(matches, start=1):
-            print ("DDD valido: {match}".format(match = match.group()))
-            exit()
-        print("DDD invalido...")
-
-    if insertBy == 2:
-        for i, line in enumerate(open("ddd.txt")):
-            for match in re.finditer(regex, line):
-                print("DDD identificado: {match}".format(match = match.group()))
-if op == 3:
-    regex = r"^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}"
-
-    insertBy = int(input("Deseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n"))
-
-    if insertBy == 1:
-        check_str = str(input("Digite o CPF: "))
-
-        matches = re.finditer(regex, check_str, re.MULTILINE)
+            if insertBy == 1:
+                check_str = str(input("Digite o DDD: "))
         
-        for matchNum, match in enumerate(matches, start=1):
-            print ("CPF valido: {match}".format(match = match.group()))
-            exit()
-        print("CPF invalido...")
-
-    if insertBy == 2:
-        for i, line in enumerate(open("cpf.txt")):
-            for match in re.finditer(regex, line):
-                print("CPF identificado: {match}".format(match = match.group()))
+                matches = re.finditer(regex, check_str, re.MULTILINE)
+                
+                for matchNum, match in enumerate(matches, start=1):
+                    print ("DDD valido: {match}".format(match = match.group()))
+            if insertBy == 2:
+                for i, line in enumerate(open("ddd.txt")):
+                    for match in re.finditer(regex, line):
+                        print("DDD identificado: {match}".format(match = match.group()))
+            if insertBy == 0:
+                    break
+    if op == 3:
+        regex = r"^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}"
+    
+        a = True
+        while(a):
+            insertBy = int(input("\nDeseja inserir de que forma?:\n1 - Unico valor\n2 - Insercao de arquivo\n0 - Para voltar\n"))
+        
+            if insertBy == 1:
+                check_str = str(input("Digite o CPF: "))
+        
+                matches = re.finditer(regex, check_str, re.MULTILINE)
+                
+                for matchNum, match in enumerate(matches, start=1):
+                    print ("CPF valido: {match}".format(match = match.group()))
+            if insertBy == 2:
+                for i, line in enumerate(open("cpf.txt")):
+                    for match in re.finditer(regex, line):
+                        print("CPF identificado: {match}".format(match = match.group()))
+            if insertBy == 0:
+                    break
+    if op == 0:
+        exit()
